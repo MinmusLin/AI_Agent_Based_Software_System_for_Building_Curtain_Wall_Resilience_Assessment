048e5b6666e7887c854872324cebc3a876624875
林继申 <linjishen.dev@bytedance.com>
2026-05-03 01:53:31 +0800

    add

--- Source_Code/icw_core_biz/internal/services/project/utils/advance.go
+++ Source_Code/icw_core_biz/internal/services/project/utils/advance.go
@@ -14,22 +14,27 @@ func PreAdvanceProject(ctx context.Context, repo *mysql.Repository, userId, proj
 	if fromProgress == consts.ProjectProgressInitializationFinished && toProgress == consts.ProjectProgressProfileFinished {
 		return nil
 	}
+
 	// 图像资产构建阶段 -> Agent 智能检测阶段
 	if fromProgress == consts.ProjectProgressProfileFinished && toProgress == consts.ProjectProgressAssetsFinished {
 		return nil
 	}
+
 	// Agent 智能检测阶段 -> 人工复核确认阶段
 	if fromProgress == consts.ProjectProgressAssetsFinished && toProgress == consts.ProjectProgressDetectFinished {
 		return nil
 	}
+
 	// 人工复核确认阶段 -> 评估报告生成阶段
 	if fromProgress == consts.ProjectProgressDetectFinished && toProgress == consts.ProjectProgressReviewFinished {
 		return nil
 	}
+
 	// 评估报告生成阶段 -> 项目已完成
 	if fromProgress == consts.ProjectProgressReviewFinished && toProgress == consts.ProjectProgressReportFinished {
 		return nil
 	}
+
 	return errors.New("invalid from progress and to progress")
 }
 
@@ -39,21 +44,26 @@ func PostAdvanceProject(ctx context.Context, repo *mysql.Repository, userId, pro
 	if fromProgress == consts.ProjectProgressInitializationFinished && toProgress == consts.ProjectProgressProfileFinished {
 		return nil
 	}
+
 	// 图像资产构建阶段 -> Agent 智能检测阶段
 	if fromProgress == consts.ProjectProgressProfileFinished && toProgress == consts.ProjectProgressAssetsFinished {
 		return nil
 	}
+
 	// Agent 智能检测阶段 -> 人工复核确认阶段
 	if fromProgress == consts.ProjectProgressAssetsFinished && toProgress == consts.ProjectProgressDetectFinished {
 		return nil
 	}
+
 	// 人工复核确认阶段 -> 评估报告生成阶段
 	if fromProgress == consts.ProjectProgressDetectFinished && toProgress == consts.ProjectProgressReviewFinished {
 		return nil
 	}
+
 	// 评估报告生成阶段 -> 项目已完成
 	if fromProgress == consts.ProjectProgressReviewFinished && toProgress == consts.ProjectProgressReportFinished {
 		return nil
 	}
+
 	return errors.New("invalid from progress and to progress")
 }
