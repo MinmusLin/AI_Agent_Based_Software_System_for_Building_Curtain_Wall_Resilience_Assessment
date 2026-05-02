1a6cb9a857210361a36cd794573ab4b564035aff
林继申 <linjishen.dev@bytedance.com>
2026-05-02 21:37:07 +0800

    add

--- Source_Code/icw_core_biz/repositories/minio/utils.go
+++ Source_Code/icw_core_biz/repositories/minio/utils.go
@@ -1,8 +1,12 @@
 package minio
 
 import (
+	"errors"
+	"fmt"
 	"net/url"
 	"strings"
+
+	"icw_core_biz/utils"
 )
 
 // normalizeEndpoint 将 Endpoint 配置标准化为 MinIO SDK 需要的 <host>:<port> 格式
@@ -16,4 +20,26 @@ func normalizeEndpoint(endpoint string) (string, bool) {
 		return "", false
 	}
 	return parsedURL.Host, parsedURL.Scheme == "https"
+}
+
+// GenDefaultAvatarKey 生成用户默认头像对象 Key
+func GenDefaultAvatarKey(emailHash string) string {
+	return fmt.Sprintf("users/avatars/%s/default.svg", strings.TrimSpace(emailHash))
+}
+
+// GenCustomAvatarKey 生成用户自定义头像对象 Key
+func GenCustomAvatarKey(emailHash string) string {
+	return fmt.Sprintf("users/avatars/%s/custom.png", strings.TrimSpace(emailHash))
+}
+
+// GenProjectThumbnailKey 生成项目缩略图对象 Key
+func GenProjectThumbnailKey(projectId uint64) (string, error) {
+	if projectId == 0 {
+		return "", errors.New("project id is invalid")
+	}
+	projectCode := utils.Encode(projectId)
+	if projectCode == "" {
+		return "", errors.New("project id is invalid")
+	}
+	return fmt.Sprintf("projects/%s/thumbnail.png", projectCode), nil
 }
