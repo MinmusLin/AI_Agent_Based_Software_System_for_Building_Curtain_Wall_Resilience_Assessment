f1621e0813a9ee07d6c51f4dae5b16cde7f00c1d
林继申 <linjishen.dev@bytedance.com>
2026-05-03 22:47:21 +0800

    add

--- Source_Code/icw_core_biz/repositories/mysql/model.go
+++ Source_Code/icw_core_biz/repositories/mysql/model.go
@@ -258,3 +258,11 @@ func ProjectImageRecordsToDTO(ctx context.Context, repo *minio.Repository, recor
 	}
 	return items, nil
 }
+
+// ProjectAssetsReadyStats 项目图像状态校验统计
+type ProjectAssetsReadyStats struct {
+	PendingImageCount  uint64
+	UploadedImageCount uint64
+	FailedImageCount   uint64
+	EmptyGroupCount    uint64
+}
