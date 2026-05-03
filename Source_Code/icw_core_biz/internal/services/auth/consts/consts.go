3ea04e3c5e4f50a16d844b2017056025802eeaf4
林继申 <linjishen.dev@bytedance.com>
2026-05-04 03:18:34 +0800

    add

--- Source_Code/icw_core_biz/internal/services/auth/consts/consts.go
+++ Source_Code/icw_core_biz/internal/services/auth/consts/consts.go
@@ -7,11 +7,14 @@ import (
 const (
 	// LoginFailureLimit 登录失败最大重试次数
 	LoginFailureLimit int = 5
-	// MinPasswordLength 密码最小字符长度
+)
+
+const (
+	// MinPasswordLength 密码最小字符数
 	MinPasswordLength int = 8
-	// MaxPasswordLength 密码最大字符长度
+	// MaxPasswordLength 密码最大字符数
 	MaxPasswordLength int = 24
-	// MaxNameLength 用户名称最大字符长度
+	// MaxNameLength 用户名称最大字符数
 	MaxNameLength int = 8
 )
 
--- Source_Code/icw_core_biz/internal/services/project/consts/consts.go
+++ Source_Code/icw_core_biz/internal/services/project/consts/consts.go
@@ -14,6 +14,25 @@ const (
 )
 
 const (
+	// ProjectNameMaxLength 项目名称最大字符数
+	ProjectNameMaxLength = 32
+	// ProjectBuildingNameMaxLength 建筑名称最大字符数
+	ProjectBuildingNameMaxLength = 32
+	// ProjectBuildingLocationMaxLength 建筑地址最大字符数
+	ProjectBuildingLocationMaxLength = 128
+	// ProjectBuildingDescriptionMaxLength 建筑描述最大字符数
+	ProjectBuildingDescriptionMaxLength = 5000
+	// ProjectKnownIssuesMaxLength 已知问题或人工先验描述最大字符数
+	ProjectKnownIssuesMaxLength = 5000
+	// ProjectAssessmentGoalMaxLength 评估目标或重点关注方向最大字符数
+	ProjectAssessmentGoalMaxLength = 5000
+	// ProjectGroupNameMaxLength 图像组名称最大字符数
+	ProjectGroupNameMaxLength = 32
+	// ProjectImageFileNameMaxLength 图像文件名最大字符数
+	ProjectImageFileNameMaxLength = 255
+)
+
+const (
 	// ThumbnailContentType 项目缩略图 MIME 类型
 	ThumbnailContentType = "image/png"
 	// ProjectImageContentType 项目图像 MIME 类型
