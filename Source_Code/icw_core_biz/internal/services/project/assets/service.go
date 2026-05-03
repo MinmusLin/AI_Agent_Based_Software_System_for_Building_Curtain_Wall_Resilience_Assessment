19b29623291b67dedb9cebcab6d2f2f172d0eb88
林继申 <linjishen.dev@bytedance.com>
2026-05-03 13:05:25 +0800

    add

--- Source_Code/icw_core_biz/internal/services/project/assets/service.go
+++ Source_Code/icw_core_biz/internal/services/project/assets/service.go
@@ -9,16 +9,8 @@ type Service struct {
 	*common.BaseService
 }
 
-type PingRequest struct{}
-
-type PingResponse struct{}
-
 func NewService(deps *common.Deps) *Service {
 	return &Service{
 		BaseService: common.NewBaseService(deps),
 	}
-}
-
-func (s *Service) Ping(_ *PingRequest, _ *PingResponse) error {
-	return nil
 }
