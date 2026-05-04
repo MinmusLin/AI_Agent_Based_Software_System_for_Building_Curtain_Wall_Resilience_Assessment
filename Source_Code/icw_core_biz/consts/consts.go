aa5f32e613e0afaa6a0719f470618d85efa6a6b3
林继申 <linjishen.dev@bytedance.com>
2026-05-04 22:03:26 +0800

    add

--- Source_Code/icw_core_biz/consts/consts.go
+++ Source_Code/icw_core_biz/consts/consts.go
@@ -3,18 +3,18 @@ package consts
 const (
 	// LogColorReset ANSI 终端颜色重置码
 	LogColorReset = "\033[0m"
-	// LogColorBoldRed ANSI 终端颜色码：红色
-	LogColorBoldRed = "\033[1;31m"
-	// LogColorBoldYellow ANSI 终端颜色码：黄色
-	LogColorBoldYellow = "\033[1;33m"
 	// LogColorBoldGreen ANSI 终端颜色码：绿色
 	LogColorBoldGreen = "\033[1;32m"
 	// LogColorBoldCyan ANSI 终端颜色码：青色
 	LogColorBoldCyan = "\033[1;36m"
-	// LogColorBoldPurple ANSI 终端颜色码：紫色
-	LogColorBoldPurple = "\033[1;35m"
 	// LogColorBoldPink ANSI 终端颜色码：粉色
 	LogColorBoldPink = "\033[1;95m"
+	// LogColorBoldYellow ANSI 终端颜色码：黄色
+	LogColorBoldYellow = "\033[1;33m"
+	// LogColorBoldRed ANSI 终端颜色码：红色
+	LogColorBoldRed = "\033[1;31m"
+	// LogColorBoldPurple ANSI 终端颜色码：紫色
+	LogColorBoldPurple = "\033[1;35m"
 	// LogColorBoldWhiteOnRed ANSI 终端颜色码：白色（红色背景）
 	LogColorBoldWhiteOnRed = "\033[1;37;41m"
 )
