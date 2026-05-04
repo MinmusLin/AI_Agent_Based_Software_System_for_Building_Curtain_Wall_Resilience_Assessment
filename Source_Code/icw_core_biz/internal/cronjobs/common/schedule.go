7650d2324ec086c8f7083efe48f4828c0180c0da
林继申 <linjishen.dev@bytedance.com>
2026-05-04 17:54:49 +0800

    add

---
+++ Source_Code/icw_core_biz/internal/cronjobs/common/schedule.go
@@ -0,0 +1,29 @@
+package common
+
+import (
+	"github.com/robfig/cron/v3"
+)
+
+// Schedule 按 Cron 表达式启动定时任务
+func (j *CronJob) Schedule(name, expression string, fn func() error) error {
+	scheduler := cron.New(
+		cron.WithSeconds(),
+		cron.WithChain(cron.SkipIfStillRunning(cron.DefaultLogger)),
+	)
+
+	if _, err := scheduler.AddFunc(expression, func() {
+		_ = j.Run(name, fn)
+	}); err != nil {
+		return err
+	}
+
+	scheduler.Start()
+
+	go func() {
+		<-j.Ctx().Done()
+		stopCtx := scheduler.Stop()
+		<-stopCtx.Done()
+	}()
+
+	return nil
+}
