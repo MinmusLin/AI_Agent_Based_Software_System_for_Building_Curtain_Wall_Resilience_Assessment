12b78cb6fb7319d36bae6d1fa568a62565031e0d
林继申 <linjishen.dev@bytedance.com>
2026-05-04 13:00:30 +0800

    add

--- Source_Code/icw_core_biz/sql/events.sql
+++ Source_Code/icw_core_biz/sql/events.sql
@@ -1,4 +1,4 @@
--- ev_project_group_images_pending_to_failed 项目图像表
+-- ev_project_group_images_pending_to_failed 项目图像上传超时状态流转任务
 CREATE DEFINER=`mysql-user`@`%` EVENT `ev_project_group_images_pending_to_failed` ON SCHEDULE EVERY 1 MINUTE STARTS '2026-05-04 12:53:27' ON COMPLETION PRESERVE ENABLE DO BEGIN
   UPDATE project_group_images
   SET status = 'failed'
