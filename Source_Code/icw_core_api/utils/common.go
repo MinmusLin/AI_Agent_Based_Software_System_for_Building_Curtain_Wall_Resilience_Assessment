4ab406f3474b4aebfdaf95f5ed6611831827546b
林继申 <linjishen.dev@bytedance.com>
2026-05-04 22:07:44 +0800

    add

--- Source_Code/icw_core_api/utils/common.go
+++ Source_Code/icw_core_api/utils/common.go
@@ -2,7 +2,6 @@ package utils
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"strings"
 
@@ -11,15 +10,6 @@ import (
 	"icw_core_api/consts"
 	"icw_core_biz/pkg/dto"
 )
-
-// JSONF 将任意结构格式化为 JSON 字符串
-func JSONF(v interface{}) string {
-	bytes, err := json.Marshal(v)
-	if err != nil {
-		return ""
-	}
-	return string(bytes)
-}
 
 // BearerToken 从 HTTP Header 中解析 Bearer Token
 // Header 格式："Authorization: Bearer <token>"
