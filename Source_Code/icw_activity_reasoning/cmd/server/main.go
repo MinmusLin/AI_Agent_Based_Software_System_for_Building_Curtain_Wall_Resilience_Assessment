fff142cf02542027800142a71f207caeb6869dff
林继申 <linjishen.dev@bytedance.com>
2026-05-05 22:36:30 +0800

    add

--- Source_Code/icw_activity_reasoning/cmd/server/main.go
+++ Source_Code/icw_activity_reasoning/cmd/server/main.go
@@ -2,23 +2,24 @@ package main
 
 import (
 	"context"
-	bizConsts "icw_core_biz/consts"
 	"net"
 	"net/rpc"
 
 	"icw_activity_reasoning/configs"
 	"icw_activity_reasoning/consts"
 	"icw_activity_reasoning/internal/detectors"
+	"icw_activity_reasoning/internal/detectors/utils"
 	"icw_activity_reasoning/internal/services"
 	"icw_activity_reasoning/internal/services/common"
 	"icw_activity_reasoning/rpc/icw_core_biz"
 	bizConfigs "icw_core_biz/configs"
-	"icw_core_biz/utils"
+	bizConsts "icw_core_biz/consts"
+	bizUtils "icw_core_biz/utils"
 )
 
 // main icw.activity.reasoning 服务入口
 func main() {
-	utils.LogInfo(consts.LogScopeInit, "", "Initializing service %s...", consts.ActivityReasoningPSM)
+	bizUtils.LogInfo(consts.LogScopeInit, "", "Initializing service %s...", consts.ActivityReasoningPSM)
 
 	ctx := context.Background()
 
@@ -26,26 +27,26 @@ func main() {
 	bizConfigs.LoadDotEnv(".env")
 	cfg, err := configs.Load()
 	if err != nil {
-		utils.LogFatal(consts.LogScopeInit, "Failed to load config: %v", err)
+		bizUtils.LogFatal(consts.LogScopeInit, "Failed to load config: %v", err)
 	}
-	utils.LogInfo(consts.LogScopeInit, "", "Config initialized successfully:\n%s", utils.FormatEnvConfig(cfg))
+	bizUtils.LogInfo(consts.LogScopeInit, "", "Config initialized successfully:\n%s", bizUtils.FormatEnvConfig(cfg))
 
 	// 初始化 icw.core.biz 服务
 	coreBizClient := icw_core_biz.NewClient(cfg.CoreBizAddr)
-	utils.LogInfo(bizConsts.LogScopeRPC, bizConsts.LogColorBoldGreen, "RPC service icw.core.biz initialized successfully")
+	bizUtils.LogInfo(bizConsts.LogScopeRPC, bizConsts.LogColorBoldGreen, "RPC service icw.core.biz initialized successfully")
 
 	// 注册 Python 检测能力
 	detectorsRegistry := detectors.RegisterDetectors(cfg.PythonBin, cfg.ReasoningWorkDir)
-	utils.LogInfo(consts.LogScopeInit, "", "Python detectors registered, waiting for calls:\n%s", detectors.FormatRegistryTable(detectorsRegistry))
+	bizUtils.LogInfo(consts.LogScopeInit, "", "Python detectors registered, waiting for calls:\n%s", utils.FormatRegistryTable(detectorsRegistry))
 
 	// 注册 RPC 服务
 	services.RegisterRPCServices(ctx, common.NewDeps(cfg, detectorsRegistry, coreBizClient))
 
 	// 运行 icw.activity.reasoning 服务
-	utils.LogInfo(bizConsts.LogScopeRPC, bizConsts.LogColorBoldGreen, "icw.activity.reasoning service starts running on %s", cfg.ActivityReasoningAddr)
+	bizUtils.LogInfo(bizConsts.LogScopeRPC, bizConsts.LogColorBoldGreen, "icw.activity.reasoning service starts running on %s", cfg.ActivityReasoningAddr)
 	listener, err := net.Listen("tcp", cfg.ActivityReasoningAddr)
 	if err != nil {
-		utils.LogFatal(consts.LogScopeInit, "Failed to run icw.activity.reasoning service: %v", err)
+		bizUtils.LogFatal(consts.LogScopeInit, "Failed to run icw.activity.reasoning service: %v", err)
 	}
 	rpc.Accept(listener)
 }
