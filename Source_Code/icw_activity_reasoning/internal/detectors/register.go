eb426c2b4eec94d0f4a408a8b7761e306eefffd5
林继申 <linjishen.dev@bytedance.com>
2026-05-05 23:07:10 +0800

    add

--- Source_Code/icw_activity_reasoning/internal/detectors/register.go
+++ Source_Code/icw_activity_reasoning/internal/detectors/register.go
@@ -7,7 +7,7 @@ import (
 
 // RegisterDetectors 注册 Python 原子检测能力
 func RegisterDetectors(pythonBin, runtimeRoot string) *common.Registry {
-	return common.NewRegistryFromMetas(pythonBin, runtimeRoot, registry())
+	return common.NewRegistry(pythonBin, runtimeRoot, registry())
 }
 
 // registry Python 原子检测能力注册表
