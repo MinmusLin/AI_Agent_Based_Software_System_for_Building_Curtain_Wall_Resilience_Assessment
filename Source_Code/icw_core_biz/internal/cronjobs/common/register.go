4bf2d368f5d26b4caba49c30d63fc6cb5c614953
林继申 <linjishen.dev@bytedance.com>
2026-05-05 21:28:28 +0800

    add

---
+++ Source_Code/icw_core_biz/internal/cronjobs/common/register.go
@@ -0,0 +1,39 @@
+package common
+
+import (
+	"icw_core_biz/utils"
+)
+
+// CronJobMeta 定时任务配置
+type CronJobMeta struct {
+	PSM         string
+	Description string
+	Cron        string
+	Start       JobFactory
+}
+
+// FormatRegistryTable 将定时任务注册表格式化为表格
+func FormatRegistryTable(cronJobs []CronJobMeta) string {
+	psmValues := make([]string, 0, len(cronJobs))
+	descriptionValues := make([]string, 0, len(cronJobs))
+	cronExpressionValues := make([]string, 0, len(cronJobs))
+	for _, item := range cronJobs {
+		psmValues = append(psmValues, item.PSM)
+		descriptionValues = append(descriptionValues, item.Description)
+		cronExpressionValues = append(cronExpressionValues, item.Cron)
+	}
+	return utils.FormatTable([]utils.TableColumn{
+		{
+			Header: "psm",
+			Values: psmValues,
+		},
+		{
+			Header: "description",
+			Values: descriptionValues,
+		},
+		{
+			Header: "cron expression",
+			Values: cronExpressionValues,
+		},
+	})
+}
--- Source_Code/icw_core_biz/internal/cronjobs/register.go
+++ Source_Code/icw_core_biz/internal/cronjobs/register.go
@@ -5,16 +5,7 @@ import (
 
 	"icw_core_biz/internal/cronjobs/common"
 	"icw_core_biz/internal/cronjobs/jobs"
-	"icw_core_biz/utils"
 )
-
-// cronJob 定时任务配置
-type cronJob struct {
-	psm         string
-	description string
-	cron        string
-	start       common.JobFactory
-}
 
 // Start 启动定时任务
 func Start(ctx context.Context, deps *common.Deps) {
@@ -22,54 +13,23 @@ func Start(ctx context.Context, deps *common.Deps) {
 		deps = &common.Deps{}
 	}
 
-	for _, item := range register(deps) {
-		if err := common.Start(ctx, deps, item.psm, item.cron, item.start); err != nil {
-			common.CronFatal("Failed to start cron job %s: %v", item.psm, err)
+	cronJobs := registry(deps)
+	common.CronInfo("Cron jobs registered, waiting for schedule:\n%s", common.FormatRegistryTable(cronJobs))
+	for _, item := range cronJobs {
+		if err := common.Start(ctx, deps, item.PSM, item.Cron, item.Start); err != nil {
+			common.CronFatal("Failed to start cron job %s: %v", item.PSM, err)
 		}
 	}
 }
 
-// register 注册定时任务
-func register(deps *common.Deps) []cronJob {
-	cronJobs := registry(deps)
-	common.CronInfo("Cron job registered, waiting for schedule:\n%s", formatRegistryTable(cronJobs))
-	return cronJobs
-}
-
 // registry 定时任务注册表
-func registry(deps *common.Deps) []cronJob {
-	return []cronJob{
+func registry(deps *common.Deps) []common.CronJobMeta {
+	return []common.CronJobMeta{
 		{
-			psm:         "icw.cron.pending_image_timeout",
-			description: "上传中图像超时失败任务",
-			cron:        deps.Config.PendingImageTimeoutJobCron,
-			start:       jobs.NewPendingImageTimeoutJob,
+			PSM:         "icw.cron.pending_image_timeout",
+			Description: "上传中图像超时失败任务",
+			Cron:        deps.Config.PendingImageTimeoutJobCron,
+			Start:       jobs.NewPendingImageTimeoutJob,
 		},
 	}
-}
-
-// formatRegistryTable 将定时任务注册表格式化为表格
-func formatRegistryTable(cronJobs []cronJob) string {
-	psmValues := make([]string, 0, len(cronJobs))
-	descriptionValues := make([]string, 0, len(cronJobs))
-	cronExpressionValues := make([]string, 0, len(cronJobs))
-	for _, item := range cronJobs {
-		psmValues = append(psmValues, item.psm)
-		descriptionValues = append(descriptionValues, item.description)
-		cronExpressionValues = append(cronExpressionValues, item.cron)
-	}
-	return utils.FormatTable([]utils.TableColumn{
-		{
-			Header: "psm",
-			Values: psmValues,
-		},
-		{
-			Header: "description",
-			Values: descriptionValues,
-		},
-		{
-			Header: "cron expression",
-			Values: cronExpressionValues,
-		},
-	})
 }
