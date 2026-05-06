62cbfa54027cae466f01f583cd87d4eb015d7be1
林继申 <linjishen.dev@bytedance.com>
2026-05-07 04:21:27 +0800

    add

--- Source_Code/icw_activity_reasoning/internal/services/register.go
+++ Source_Code/icw_activity_reasoning/internal/services/register.go
@@ -7,31 +7,18 @@ import (
 
 	"icw_activity_reasoning/internal/services/common"
 	"icw_activity_reasoning/internal/services/reasoning"
-	"icw_common/consts"
 	"icw_common/gen/activity/reasoning"
-	"icw_common/utils"
+	"icw_common/rpc"
 )
 
 // RegisterRPCServices 注册 gRPC 服务
 func RegisterRPCServices(ctx context.Context, serviceDeps *common.Deps, grpcServer *grpc.Server) {
-	registeredMethods := make([]common.RegisteredRPCMethodMeta, 0)
-	for _, meta := range registry(ctx, serviceDeps) {
-		methods, err := common.ResolveRPCMethods(meta)
-		if err != nil {
-			utils.LogFatal(consts.LogScopeRPC, "Failed to register RPC service %s: %v", meta.Name, err)
-		}
-		if meta.Register == nil {
-			utils.LogFatal(consts.LogScopeRPC, "Failed to register RPC service %s: register function is nil", meta.Name)
-		}
-		meta.Register(grpcServer, meta.Service)
-		registeredMethods = append(registeredMethods, methods...)
-	}
-	utils.LogFatal(consts.LogScopeRPC, "RPC methods registered, waiting for requests:\n%s", common.FormatRegistryTable(registeredMethods))
+	rpc.RegisterServices(grpcServer, registry(ctx, serviceDeps))
 }
 
 // registry RPC 服务注册表
-func registry(ctx context.Context, serviceDeps *common.Deps) []common.RPCServiceMeta {
-	return []common.RPCServiceMeta{
+func registry(ctx context.Context, serviceDeps *common.Deps) []rpc.ServiceMeta {
+	return []rpc.ServiceMeta{
 		{
 			Name:        "ReasoningService",
 			Description: "推理能力服务",
@@ -39,7 +26,7 @@ func registry(ctx context.Context, serviceDeps *common.Deps) []common.RPCService
 			Register: func(server grpc.ServiceRegistrar, service interface{}) {
 				reasoningpb.RegisterReasoningServiceServer(server, service.(*reasoning.Service))
 			},
-			Methods: []common.RPCMethodMeta{
+			Methods: []rpc.MethodMeta{
 				{Name: "Start", Description: "启动推理任务"},
 			},
 		},
