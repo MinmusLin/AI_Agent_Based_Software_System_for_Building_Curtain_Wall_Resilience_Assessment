1d504c2c8921d1b3f0f495a3c78ca92291336b43
林继申 <linjishen.dev@bytedance.com>
2026-05-06 15:18:54 +0800

    add

--- Source_Code/icw_core_biz/internal/services/common/log.go
+++ Source_Code/icw_core_biz/internal/services/common/log.go
@@ -1,61 +1,9 @@
 package common
 
 import (
-	"reflect"
-	"strings"
-	"time"
-
-	"icw_core_biz/consts"
-	"icw_core_biz/utils"
+	"icw_common/consts"
+	"icw_common/utils"
 )
-
-// rpcLog 输出 RPC 请求日志
-func rpcLog(method string, req, resp interface{}, start time.Time, err error) {
-	requestId := getRequestId(req)
-	if requestId == "" {
-		requestId = "-"
-	}
-
-	if utils.IsEmptyError(err) {
-		RpcInfo("[%s] [%s] cost=%s req=%s resp=%s", method, requestId, time.Since(start), utils.JSONF(req), utils.JSONF(resp))
-		return
-	}
-	RpcError("[%s] [%s] cost=%s req=%s resp=%s err=%s", method, requestId, time.Since(start), utils.JSONF(req), utils.JSONF(resp), utils.FormatErrorLog(err))
-}
-
-// getRequestId 从 RPC 元数据中获取请求 ID
-func getRequestId(req interface{}) string {
-	if req == nil {
-		return ""
-	}
-
-	reqValue := reflect.ValueOf(req)
-	if reqValue.Kind() != reflect.Ptr || reqValue.IsNil() {
-		return ""
-	}
-
-	elem := reqValue.Elem()
-	if elem.Kind() != reflect.Struct {
-		return ""
-	}
-
-	metaField := elem.FieldByName("Meta")
-	if !metaField.IsValid() || metaField.Kind() != reflect.Ptr || metaField.IsNil() {
-		return ""
-	}
-
-	metaValue := metaField.Elem()
-	if metaValue.Kind() != reflect.Struct {
-		return ""
-	}
-
-	requestIdField := metaValue.FieldByName("RequestId")
-	if !requestIdField.IsValid() || requestIdField.Kind() != reflect.String {
-		return ""
-	}
-
-	return strings.TrimSpace(requestIdField.String())
-}
 
 // RpcInfo 输出标准 RPC 日志
 func RpcInfo(format string, args ...interface{}) {
