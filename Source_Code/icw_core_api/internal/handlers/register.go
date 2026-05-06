79a0e35b401a90f12158bb342e4e9218951bb322
林继申 <linjishen.dev@bytedance.com>
2026-05-06 13:04:02 +0800

    add

--- Source_Code/icw_core_api/internal/handlers/register.go
+++ Source_Code/icw_core_api/internal/handlers/register.go
@@ -5,7 +5,6 @@ import (
 
 	"icw_common/consts"
 	"icw_common/utils"
-
 	"icw_core_api/configs"
 	"icw_core_api/internal/handlers/auth"
 	"icw_core_api/internal/handlers/common"
--- Source_Code/icw_core_api/internal/handlers/user/upload_avatar.go
+++ Source_Code/icw_core_api/internal/handlers/user/upload_avatar.go
@@ -4,7 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 
 	"icw_common/gen/core/biz"
-
 	"icw_core_api/internal/response"
 	"icw_core_api/rpc/icw_core_biz/user"
 	"icw_core_api/utils"
