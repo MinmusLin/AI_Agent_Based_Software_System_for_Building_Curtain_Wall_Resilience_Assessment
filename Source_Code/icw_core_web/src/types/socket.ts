a5e6a61f29bf262ba5449d0f3f94df12ce312878
林继申 <linjishen.dev@bytedance.com>
2026-05-07 11:15:07 +0800

    add

--- Source_Code/icw_core_web/src/types/socket.ts
+++ Source_Code/icw_core_web/src/types/socket.ts
@@ -1,4 +1,4 @@
-import type { ProjectDetectionMainStatus, ProjectDetectionSubStatus, ProjectEventType } from '@/types/common';
+import type { ProjectEventType } from '@/types/common';
 import type { ProjectImage } from '@/types/project/assets';
 
 export interface CreateSocketTicketResponse {
@@ -10,16 +10,4 @@ export interface ProjectImageStatusChangedMessage {
   type: ProjectEventType;
   project_id: string;
   image: ProjectImage;
-}
-
-export interface ProjectDetectionTaskStatusChangedMessage {
-  type: ProjectEventType;
-  project_id: string;
-  image_uuid: string;
-  node_code: string;
-  main_task_id: string;
-  main_status: ProjectDetectionMainStatus;
-  sub_task_id: string;
-  sub_status: ProjectDetectionSubStatus;
-  occurred_at: string;
 }
