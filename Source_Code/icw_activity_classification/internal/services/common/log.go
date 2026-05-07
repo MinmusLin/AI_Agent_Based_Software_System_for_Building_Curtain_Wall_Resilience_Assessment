c651f3927764cce2c4a506d58b497f747a6ec288
林继申 <linjishen.dev@bytedance.com>
2026-05-07 09:14:39 +0800

    add

--- Source_Code/icw_activity_classification/internal/services/common/log.go
+++ Source_Code/icw_activity_classification/internal/services/common/log.go
@@ -12,7 +12,7 @@ import (
 // ClassificationInfo 输出标准分类能力日志
 func ClassificationInfo(requestId, taskUuid, imageUuid string, taskCodes []string, cost time.Duration) {
 	utils.LogInfo(consts.LogScopeClassification, consts.LogColorBoldPurple, "[%s] %s %13v %s task_uuid=%s image_uuid=%s task_codes=%s",
-		requestId,
+		utils.If[string](requestId == "", "-", requestId),
 		consts.LogColorBoldBlackOnWhite, cost, consts.LogColorReset,
 		taskUuid,
 		imageUuid,
@@ -23,7 +23,7 @@ func ClassificationInfo(requestId, taskUuid, imageUuid string, taskCodes []strin
 // ClassificationError 输出失败分类能力日志
 func ClassificationError(requestId, taskUuid, imageUuid string, taskCodes []string, cost time.Duration, err error) {
 	utils.LogError(consts.LogScopeClassification, "[%s] %s %13v %s task_uuid=%s image_uuid=%s task_codes=%s err=%s",
-		requestId,
+		utils.If[string](requestId == "", "-", requestId),
 		consts.LogColorBoldBlackOnWhite, cost, consts.LogColorReset,
 		taskUuid,
 		imageUuid,
@@ -35,7 +35,7 @@ func ClassificationError(requestId, taskUuid, imageUuid string, taskCodes []stri
 // CallbackInfo 输出标准回调日志
 func CallbackInfo(requestId, taskUuid, imageUuid, status string, start time.Time) {
 	utils.LogInfo(consts.LogScopeCallback, consts.LogColorBoldBlue, "[%s] %s %13v %s [CLASSIFICATION] task_uuid=%s image_uuid=%s status=%s%s%s",
-		requestId,
+		utils.If[string](requestId == "", "-", requestId),
 		consts.LogColorBoldBlackOnWhite, time.Since(start), consts.LogColorReset,
 		taskUuid,
 		imageUuid,
@@ -46,7 +46,7 @@ func CallbackInfo(requestId, taskUuid, imageUuid, status string, start time.Time
 // CallbackError 输出失败回调日志
 func CallbackError(requestId, taskUuid, imageUuid, status string, start time.Time, err error) {
 	utils.LogError(consts.LogScopeCallback, "[%s] %s %13v %s [CLASSIFICATION] task_uuid=%s image_uuid=%s status=%s%s%s err=%s",
-		requestId,
+		utils.If[string](requestId == "", "-", requestId),
 		consts.LogColorBoldBlackOnWhite, time.Since(start), consts.LogColorReset,
 		taskUuid,
 		imageUuid,
