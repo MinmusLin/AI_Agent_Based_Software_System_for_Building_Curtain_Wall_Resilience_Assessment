a25ee6a2b7693e0d01adad54f17b9c16d33a49b0
林继申 <linjishen.dev@bytedance.com>
2026-05-07 17:23:47 +0800

    add

--- Source_Code/icw_core_biz/repositories/mysql/model/error.go
+++ Source_Code/icw_core_biz/repositories/mysql/model/error.go
@@ -11,4 +11,12 @@ var (
 	ErrProjectGroupCannotDeleteLast = errors.New("project must keep at least one group")
 	// ErrUnsupportedDetectionTaskCode 不支持的原子检测能力代码
 	ErrUnsupportedDetectionTaskCode = errors.New("unsupported detection task code")
+	// ErrMoveProjectGroupDirectionConflict 移动图像组时不能同时置顶和置底
+	ErrMoveProjectGroupDirectionConflict = errors.New("move to first and move to last cannot both be true")
+	// ErrProjectDetectionTaskStatusInvalid 项目图像检测主任务状态非法
+	ErrProjectDetectionTaskStatusInvalid = errors.New("project detection task status is invalid")
+	// ErrProjectDetectionSubTaskStatusInvalid 项目图像检测子任务状态非法
+	ErrProjectDetectionSubTaskStatusInvalid = errors.New("project detection sub task status is invalid")
+	// ErrProjectDetectionSummaryTaskStatusInvalid 项目图像检测总结任务状态非法
+	ErrProjectDetectionSummaryTaskStatusInvalid = errors.New("project detection summary task status is invalid")
 )
