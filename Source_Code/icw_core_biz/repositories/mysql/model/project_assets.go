0d513278414e26cd73546e67bd27fa513134da59
林继申 <linjishen.dev@bytedance.com>
2026-05-07 22:51:53 +0800

    add

--- Source_Code/icw_core_biz/repositories/mysql/model/project_assets.go
+++
@@ -1,9 +0,0 @@
-package model
-
-// ProjectAssetsReadyStats 项目图像状态校验
-type ProjectAssetsReadyStats struct {
-	PendingImageCount  uint64
-	UploadedImageCount uint64
-	FailedImageCount   uint64
-	EmptyGroupCount    uint64
-}
