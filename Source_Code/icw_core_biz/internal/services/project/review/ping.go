00a509cd6acd08e50bcc22f138db4d10a9a06e42
林继申 <linjishen.dev@bytedance.com>
2026-05-08 14:41:17 +0800

    add

--- Source_Code/icw_core_biz/internal/services/project/review/ping.go
+++ Source_Code/icw_core_biz/internal/services/project/review/ping.go
@@ -8,13 +8,5 @@ import (
 
 // Ping .
 func (s *Service) Ping(ctx context.Context, req *bizpb.PingReviewRequest) (*bizpb.PingReviewResponse, error) {
-	resp := &bizpb.PingReviewResponse{}
-	err := s.CallRPC(ctx, req, func() error {
-		return s.ping(req, resp)
-	})
-	return resp, err
-}
-
-func (s *Service) ping(_ *bizpb.PingReviewRequest, _ *bizpb.PingReviewResponse) error {
-	return nil
+	return nil, nil
 }
