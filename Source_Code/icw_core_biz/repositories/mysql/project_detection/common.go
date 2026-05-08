39d32af883b4c7c4d25c2226b3f75a3faeb39d0c
林继申 <linjishen.dev@bytedance.com>
2026-05-08 17:54:02 +0800

    add

---
+++ Source_Code/icw_core_biz/repositories/mysql/project_detection/common.go
@@ -0,0 +1,56 @@
+package project_detection
+
+import (
+	"strings"
+
+	"icw_common/enum"
+	"icw_common/gen/activity"
+	"icw_common/gen/core/biz"
+
+	"icw_core_biz/repositories/mysql/model"
+)
+
+// NormalizeDetectionTaskCodes 标准化图像检测子任务代码
+func NormalizeDetectionTaskCodes(taskCodes []string) ([]string, error) {
+	normalized := make([]string, 0, len(taskCodes))
+	seen := map[activitypb.DetectionTaskCode_Value]bool{}
+	for _, taskCode := range taskCodes {
+		if strings.TrimSpace(taskCode) == "" {
+			continue
+		}
+		code := enum.ParseDetectionTaskCode(taskCode)
+		if code == activitypb.DetectionTaskCode_Unknown {
+			return nil, model.ErrUnsupportedDetectionTaskCode
+		}
+		if seen[code] {
+			continue
+		}
+		normalized = append(normalized, enum.DetectionTaskCodeString(code))
+		seen[code] = true
+	}
+	return normalized, nil
+}
+
+// ClassificationNodeStatus 根据图像检测主任务状态推导图像检测分类任务状态
+func ClassificationNodeStatus(task *model.ProjectDetectionTaskRecord) bizpb.ProjectDetectionSubTaskStatus_Value {
+	switch task.Status {
+	case bizpb.ProjectDetectionTaskStatus_Classifying:
+		return bizpb.ProjectDetectionSubTaskStatus_Pending
+	case bizpb.ProjectDetectionTaskStatus_Detecting,
+		bizpb.ProjectDetectionTaskStatus_Summarizing,
+		bizpb.ProjectDetectionTaskStatus_Succeeded:
+		return bizpb.ProjectDetectionSubTaskStatus_Succeeded
+	case bizpb.ProjectDetectionTaskStatus_Failed:
+		if !task.CorrosionShouldExecute &&
+			!task.CrackShouldExecute &&
+			!task.StainShouldExecute &&
+			!task.FlatnessShouldExecute &&
+			!task.SpallingShouldExecute &&
+			!task.SummaryShouldExecute {
+			return bizpb.ProjectDetectionSubTaskStatus_Failed
+		}
+		return bizpb.ProjectDetectionSubTaskStatus_Succeeded
+	default:
+		return bizpb.ProjectDetectionSubTaskStatus_Unknown
+	}
+}
--- Source_Code/icw_core_biz/repositories/mysql/utils/common.go
+++ Source_Code/icw_core_biz/repositories/mysql/utils/common.go
@@ -10,12 +10,7 @@ import (
 
 	"github.com/go-sql-driver/mysql"
 
-	"icw_common/enum"
-	"icw_common/gen/activity"
-	"icw_common/gen/core/biz"
-
 	"icw_core_biz/configs"
-	"icw_core_biz/repositories/mysql/model"
 )
 
 // MySQLDSN 根据配置生成 MySQL 连接 DSN
@@ -128,6 +123,14 @@ func NullUint64(value sql.NullInt64) uint64 {
 	return uint64(value.Int64)
 }
 
+// NullTimeString 将 sql.NullTime 转换为时间字符串
+func NullTimeString(value sql.NullTime) string {
+	if !value.Valid {
+		return ""
+	}
+	return value.Time.Format(time.DateTime)
+}
+
 // CheckRowsAffected 检查 SQL 执行结果是否至少影响一行
 func CheckRowsAffected(result sql.Result, err error) error {
 	if err != nil {
@@ -141,49 +144,4 @@ func CheckRowsAffected(result sql.Result, err error) error {
 		return sql.ErrNoRows
 	}
 	return nil
-}
-
-// NormalizeDetectionTaskCodes 标准化图像检测子任务代码
-func NormalizeDetectionTaskCodes(taskCodes []string) ([]string, error) {
-	normalized := make([]string, 0, len(taskCodes))
-	seen := map[activitypb.DetectionTaskCode_Value]bool{}
-	for _, taskCode := range taskCodes {
-		if strings.TrimSpace(taskCode) == "" {
-			continue
-		}
-		code := enum.ParseDetectionTaskCode(taskCode)
-		if code == activitypb.DetectionTaskCode_Unknown {
-			return nil, model.ErrUnsupportedDetectionTaskCode
-		}
-		if seen[code] {
-			continue
-		}
-		normalized = append(normalized, enum.DetectionTaskCodeString(code))
-		seen[code] = true
-	}
-	return normalized, nil
-}
-
-// ClassificationNodeStatus 根据图像检测主任务状态推导图像检测分类任务状态
-func ClassificationNodeStatus(task *model.ProjectDetectionTaskRecord) bizpb.ProjectDetectionSubTaskStatus_Value {
-	switch task.Status {
-	case bizpb.ProjectDetectionTaskStatus_Classifying:
-		return bizpb.ProjectDetectionSubTaskStatus_Pending
-	case bizpb.ProjectDetectionTaskStatus_Detecting,
-		bizpb.ProjectDetectionTaskStatus_Summarizing,
-		bizpb.ProjectDetectionTaskStatus_Succeeded:
-		return bizpb.ProjectDetectionSubTaskStatus_Succeeded
-	case bizpb.ProjectDetectionTaskStatus_Failed:
-		if !task.CorrosionShouldExecute &&
-			!task.CrackShouldExecute &&
-			!task.StainShouldExecute &&
-			!task.FlatnessShouldExecute &&
-			!task.SpallingShouldExecute &&
-			!task.SummaryShouldExecute {
-			return bizpb.ProjectDetectionSubTaskStatus_Failed
-		}
-		return bizpb.ProjectDetectionSubTaskStatus_Succeeded
-	default:
-		return bizpb.ProjectDetectionSubTaskStatus_Unknown
-	}
 }
