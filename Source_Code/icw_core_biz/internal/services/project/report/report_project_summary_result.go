a75d5c84fcbf2e7e97e7f373b50a4f65d7f22a9e
林继申 <linjishen.dev@bytedance.com>
2026-05-09 09:12:44 +0800

    add

--- Source_Code/icw_core_biz/internal/services/project/report/report_project_summary_result.go
+++ Source_Code/icw_core_biz/internal/services/project/report/report_project_summary_result.go
@@ -6,6 +6,8 @@ import (
 	"icw_common/gen/activity"
 	"icw_common/gen/core/biz"
 	"icw_common/rpc/error"
+
+	"icw_core_biz/internal/services/project/events"
 )
 
 // ReportProjectSummaryResult 上报项目总结结果
@@ -18,12 +20,33 @@ func (s *Service) ReportProjectSummaryResult(ctx context.Context, req *bizpb.Rep
 }
 
 func (s *Service) reportProjectSummaryResult(req *bizpb.ReportProjectSummaryResultRequest) error {
+	var reportStatus bizpb.ProjectReportStatus_Value
 	switch req.Status {
 	case activitypb.DetectionStatus_Succeeded:
-		return nil
+		reportStatus = bizpb.ProjectReportStatus_Succeeded
 	case activitypb.DetectionStatus_Failed:
-		return nil
+		reportStatus = bizpb.ProjectReportStatus_Failed
 	default:
-		return rpc_error.BadRequestDefault("summary status is invalid")
+		return rpc_error.BadRequestDefault("project summary status is invalid")
 	}
+
+	// 按项目 ID 更新项目评估报告结果
+	report, err := s.MySQL().UpdateProjectReportResult(s.Ctx(), req.ProjectId, reportStatus, req.ResultJson)
+	if err != nil {
+		return err
+	}
+
+	if report != nil {
+		// 发布项目评估报告状态变化事件
+		events.PublishProjectReportStatusChangedEvent(
+			s.Ctx(),
+			s.RocketMQ(),
+			report.UserId,
+			report.ProjectId,
+			report.Uuid,
+			report.Status,
+		)
+	}
+
+	return err
 }
